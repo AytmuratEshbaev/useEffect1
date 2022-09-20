9a2066641a6b0d8fb163585ef9d78f39251c5976
Aytmurat Eshbaev <a.eshbaev1@gmail.com>
2022-09-20 18:35:15 +0500

    finish

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function ClickedBtn() {
-    const [items, setItems] = useState([0, 1]);
-    const [hoverItem, setHoverItem] = useState('none')
-    const addBtn = () => {
-        setItems(items => [...items, items.length]);
+function Info() {
+    const [name, setName] = useState('y');
+    const [luckyNumber, setLuckyNumber] = useState(~~(Math.random() * 100));
+    const [count, setCount] = useState(1)
+
+    const nameRef = useRef()
+    const numRef = useRef()
+    const countRef = useRef()
+
+
+    useEffect(() => {
+        const nameElement = nameRef.current;
+        const numElement = numRef.current;
+        const changesElement = countRef.current;
+        nameElement.textContent = name;
+        numElement.textContent = luckyNumber;
+        changesElement.textContent = count;
+    })
+
+
+    function changeName() {
+        setName(name + 'y')
+        setCount(count + 1)
     }
-    const hovered = (e) => {
-        setHoverItem(e.target.childNodes[1].textContent)
+
+    function changeNumber() {
+        setLuckyNumber(~~(Math.random() * 100))
     }
-    const notHover = () => {
-        setHoverItem('none');
-    }
+
+
     return (
         <div className='wrapper'>
-            <button onClick={addBtn}>Add</button>
-            <p>Clicked button number: <span>{hoverItem}</span> </p>
-            <ul>
-                {items.map((item, index) =>
-                    <li key={index} onMouseOver={hovered} onMouseLeave={notHover}>Item <span>{item}</span></li>
-                )}
-            </ul>
-
+            <p>Welcome Cool Gu<span ref={nameRef}></span>!</p>
+            <p>Your lucky number is <span ref={numRef}></span></p>
+            <p>Name has changed <span ref={countRef}></span> times</p>
+            <button onClick={changeName}>Change Name</button>
+            <button onClick={changeNumber}>Change Lucky Number</button>
         </div>
     )
 }
@@ -31,7 +46,7 @@ function ClickedBtn() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ClickedBtn />
+        <Info />
     </React.StrictMode>
 );
 
